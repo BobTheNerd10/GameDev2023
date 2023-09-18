6dd014060d67643c14615558d5b04faf130eccde
BobTheNerd10 <Bob_The_Nerd@shaw.ca>
2023-09-18 09:33:06 -0700

    waslajaklajklsfdjkl

--- public/t1a01-BasicWeb-Curtis/t1a01.js
+++ public/t1a01-BasicWeb-Curtis/t1a01.js
@@ -1,1 +1,7 @@
-console.log("Done")
+
+
+let spanT = document.getElementById("spanT");
+let spanH = document.getElementById("spanH");
+let spanI = document.getElementById("spanI");
+let spanS = document.getElementById("spanS");
+
