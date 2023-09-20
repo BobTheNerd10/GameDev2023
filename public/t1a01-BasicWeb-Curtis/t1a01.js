95a9f9a3508c38019d53e4e785c75045478615f4
BobTheNerd10 <Bob_The_Nerd@shaw.ca>
2023-09-20 14:51:19 -0700

    wayyy too much work

--- public/t1a01-BasicWeb-Curtis/t1a01.js
+++
@@ -1,7 +0,0 @@
-
-
-let spanT = document.getElementById("spanT");
-let spanH = document.getElementById("spanH");
-let spanI = document.getElementById("spanI");
-let spanS = document.getElementById("spanS");
-
