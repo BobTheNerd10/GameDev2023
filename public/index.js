b2c3393e8fa9eacd7ef2d04fe36ce98953cf44a9
BobTheNerd10 <Bob_The_Nerd@shaw.ca>
2023-09-27 10:33:49 -0700

    COMMIT QUICK

--- public/index.js
+++ public/index.js
@@ -3,54 +3,95 @@ const creatorName = "Curtis"
 
 class AssignmentEntry {
 
-    constructor(assignmentId, assignmentName, isWip = false, isReleased = false, linkOveride = null){
+    constructor(assignmentId, assignmentName, isShown = true, isWip = false, isSubmitted = false, linkOveride = null)
+    {
         
-        this.assignmentId = assignmentId; // tXaYY
+        this.assignmentId = assignmentId.toUpperCase(); // tXaYY
         this.assignmentName = assignmentName; // whatever ellis writes. Varies between kebab case and pascal case D:
 
         this.fullAssignmentName = this.assignmentId + "-" + this.assignmentName + "-" + creatorName // How ellis wants the assignments to be formatted
         
-
+        this.isShown = isShown
         this.isWip = isWip;
-        this.isReleased = isReleased;
+        this.isSubmitted = isSubmitted;
 
         // Just in case it's NOT an index.html within a folder of the same name (eg, work-not-screen-time)
         this.fullLink = linkOveride ? linkOveride : this.fullAssignmentName + "/index.html"
     }
 
     
-    stringTest(isTodoList = false){
-        if(this.isReleased){
-            htmlElement.innerHTML += `<a href="${this.fullLink}">${this.fullAssignmentName}`
-            if (this.isWip) {
-                htmlElement.innerHTML += ` WIP`
+    getLinkFormat(isTodoList = false)
+    {
+
+        let formattedLink = ""
+
+        if(this.isShown)
+        {
+            if(isTodoList)
+            {
+                if(this.isSubmitted)
+                {
+                    if(this.isWip)
+                    {
+                        
+                    }
+                    else if(!this.isWip)
+                    {
+                        return
+                    }
+                }
+                else if(this.isSubmitted)
+                {
+                    if(this.isWip)
+                    {
+                        
+                    }
+                    else if(!this.isWip)
+                    {
+                        return
+                    }
+                }
+
             }
-            htmlElement.innerHTML += `</a>`
+            else if(!isTodoList)
+            {
+                if(this.isSubmitted)
+                {
+                    formattedLink += `<a href="${this.fullLink}">${this.fullAssignmentName}</a>`
+                    
+                    if (this.isWip) 
+                    {
+                        formattedLink += ` WIP`
+                    }
+                }
+                else if (this.isWip) 
+                {
+                    formattedLink += `<p>${this.assignmentId.toUpperCase()} is a work in progress<p>`
+                }
+            }
+
         }
-        else if (this.isWip) {
-            htmlElement.innerHTML += `<p>${this.assignmentId.toUpperCase()} is a work in progress<p>`
-        }
-        else {
-            return
-        }
+
+        return formattedLink
     }
 }
 
 
-let assignmentListData = [
-    new AssignmentEntry(assignmentId = "t1a01", assignmentName = "BasicWeb",             isWip = true,   isReleased = false  ),
-    new AssignmentEntry(assignmentId = "t1a02", assignmentName = "lists-tables",         isWip = true,   isReleased = false  ),
-    new AssignmentEntry(assignmentId = "t1a03", assignmentName = "forms",                isWip = true,   isReleased = false  ),
-    new AssignmentEntry(assignmentId = "t1a04", assignmentName = "css",                  isWip = true,   isReleased = false  ),
-  //new AssignmentEntry(assignmentId = "t1z00", assignmentName = "Work-Not-Screen-Time", isWip = false,  isReleased = true,  linkOveride = "Work-Not-Screen-Time-Curtis.html"),
-    new AssignmentEntry(assignmentId = "t1z01", assignmentName = "first-javascript",     isWip = true,   isReleased = false  ),
-    new AssignmentEntry(assignmentId = "t1z02", assignmentName = "object-zombie",        isWip = true,   isReleased = true   ),
-    new AssignmentEntry(assignmentId = "t1z03", assignmentName = "array-zombie",         isWip = true,   isReleased = false  ),
+let assignmentListData = 
+[
+    new AssignmentEntry(assignmentId = "t1a01", assignmentName = "BasicWeb",             isShown = true,  isWip = true,   isSubmitted = false  ),
+    new AssignmentEntry(assignmentId = "t1a02", assignmentName = "lists-tables",         isShown = true,  isWip = true,   isSubmitted = false  ),
+    new AssignmentEntry(assignmentId = "t1a03", assignmentName = "forms",                isShown = true,  isWip = true,   isSubmitted = false  ),
+    new AssignmentEntry(assignmentId = "t1a04", assignmentName = "css",                  isShown = true,  isWip = true,   isSubmitted = false  ),
+    new AssignmentEntry(assignmentId = "t1z00", assignmentName = "Work-Not-Screen-Time", isShown = true,  isWip = false,  isSubmitted = true,  linkOveride = "Work-Not-Screen-Time-Curtis.html"),
+    new AssignmentEntry(assignmentId = "t1z01", assignmentName = "first-javascript",     isShown = true,  isWip = true,   isSubmitted = false  ),
+    new AssignmentEntry(assignmentId = "t1z02", assignmentName = "object-zombie",        isShown = true,  isWip = true,   isSubmitted = true   ),
+    new AssignmentEntry(assignmentId = "t1z03", assignmentName = "array-zombie",         isShown = true,  isWip = true,   isSubmitted = false  ),
 ]
 
 
 let assignmentList = document.getElementById("assignmentList")
 
 assignmentListData.forEach(function(assignmentEntryToAdd){
-    assignmentEntryToAdd.addToAssignmentList(assignmentList)
+        assignmentList.innerHTML += assignmentEntryToAdd.getLinkFormat(false)
 })
