e6ae4a1cf571e0bdb9c3c0a50f0954299c1f4ac6
BobTheNerd10 <Bob_The_Nerd@shaw.ca>
2023-12-11 11:33:00 -0800

    lots and lots of platformer work!

--- public/zFinalProject/platformer.js
+++ public/zFinalProject/platformer.js
@@ -1,34 +1,62 @@
 
+// For time based things
+const sleep = ms => new Promise(r => setTimeout(r, ms)); 
+
+
+
+let gameElementDiv = document.getElementById('gameElements')
+
+let gameElements = gameElementDiv.childNodes
+
+
+
+let backgroundElementDiv = document.getElementById('backgroundElements')
+
+let backgroundElements = backgroundElementDiv.childNodes
+
+
+
+// Global values
+let airFriction     // Think back to the coefficient of friction from physics class, it's like that
+let groundFriction  // Think back to the coefficient of friction from physics class, it's like that
+let cameraX = 0 // Leftmost point of scene
+let cameraY = 0 // Bottommost point of scene
+let cameraZoom = 1
 
 
 
 
-let gameObjectDiv = document.getElementById('gameObjects')
+//============================================
+// Everything below this point is in functions
+//============================================
 
-for(gameElement of gameObjectDiv.childNodes)
+
+
+
+
+async function gameUpdate()
 {
-    new GameObject()
+    for(let gameElement of gameElements)
+    {
+
+    }
 }
 
 
-class GameObject
+
+async function outsideSceneAwake()
+{
+    gameUpdateLoop = setInterval(gameUpdate, 1)
+}
+
+
+
+async function bossSceneAwake()
 {
 
 }
 
 
-
-
-
-function outsideSceneAwake()
-{
-    // Fill this function later
-}
-
-function bossSceneAwake()
-{
-    // Fill this function later
-}
 
 
 
@@ -38,93 +66,165 @@ function bossSceneAwake()
 /*
 
 
+Elements should be <img> but anything works
+
+GameElement attributes:
+    x
+    y
+    xVelocity
+    yVelocity
+    width
+    height
+    facing ("right" or "left")
+    src (for the image sprite)
+    onUpdate (list of JS functions that's called every frame)
+    onCollision (takes the colliding object as well as any arguments in the onCollision list as an input, and returns how it's modified as an output)
 
 
-Create game objects within a div in the platformer scene
-Get all the child nodes in the div and make them instances in a gameObject class
-refer to the element's attributes to give it properties
-handle all the stuff in this script
-
-have a variable specifically for the player
-
-
-
-
-
-
-
-
-
-
-
-
-// global variables here
-let myGlobalX = 0
-let myClock
-
-
-function myHitOther(my1, my2)
-{
-    left1      = parseInt(document.getElementById(my1).style.left)
-    right1     = left1 + parseInt(document.getElementById(my1).style.width)
-    top1       = parseInt(document.getElementById(my1).style.top)
-    bottom1    = top1 + parseInt(document.getElementById(my1).style.height)
-    left2      = parseInt(document.getElementById(my2).style.left)
-    right2     = left2 + parseInt(document.getElementById(my2).style.width)
-    top2       = parseInt(document.getElementById(my2).style.top)
-    bottom2    = top2 + parseInt(document.getElementById(my2).style.height)   
-
-    if 
-    (
-        ( right1  >= left2  ) &&
-        ( bottom1 >= top2   ) &&
-        ( left1   <= right2 ) &&
-        ( top1    <= bottom2) 
-    )
-    {
-    return true
-    }
-}
-
-function myCheckHit()
-{
-    if ( myHitOther('myImg01', 'myImg02'))
-    {
-        location = 'microgames.html' 
-    }
-
-    if ( myHitOther('myImg01', 'myImg03'))
-    {
-        alert('you lose 3')
-    }
-
-    // more if statements here
-}
-
-
-
-
-// Auto background left function
-{
-    clearInterval(myClock)
-    let myTemp = '100px 0px'
-    myClock = setInterval(function(){
-        myGlobalX -= 3
-        myTemp = myGlobalX + 'px 0px'
-        document.getElementById('myBody').style.backgroundPosition = myTemp
-    }, 10)
-}
-
-
-
-// the function that makes the guy move right
-{
-    document.getElementById('myImg01').style.left = parseInt(document.getElementById('myImg01').style.left) +
-    10 + 'px'
-        myCheckHit()
-}
-
-
-
+background element attributes:
+    x
+    y
+    z (for parallax effect. Higher = further back. Not same as zindex)
+    width (when the bg element is at 0 z)
+    height (when the bg element is at 0 z)
+    src
 
 */
+
+
+
+
+
+
+
+
+
+/* 
+===============================================================================================================================================================
+Update components   Update components   Update components   Update components   Update components   Update components   Update components   Update components 
+===============================================================================================================================================================    
+Called every update/tick/frame
+*/
+
+let allUpdateComponents = 
+{
+    "evaluatePhysics" : evaluatePhysics,
+    "evaluateCollisions" : evaluateCollisions,
+    "player" : playerUpdate,
+    "boss" : bossUpdate,
+    "wrapsAroundScreen" : wrapsAroundScreen,
+}
+
+
+// The element with this will have gravity applied, and other global physics things 
+function evaluatePhysics() 
+{
+
+}
+
+
+
+// The element with this evaluates all other elements to see if it's colliding with any of them, and calls their onCollision functions if it is
+function evaluateCollisions() 
+{
+
+}
+
+
+
+// Evaluates player controls and health(?)
+function playerUpdate() 
+{
+
+}
+
+
+
+// Evaluates boss AI 
+function bossUpdate() 
+{
+
+}
+
+
+
+// Will teleport to the other side of the screen (just off screen) when it goes off screen, maintaining the velocity
+function wrapsAroundScreen() 
+{
+
+}
+
+
+
+/* 
+==============================================================================================================================================================
+Collision components   Collision components   Collision components   Collision components   Collision components   Collision components   Collision components
+============================================================================================================================================================== 
+Called whenever an object with the "evaluateCollisions" component collides with another element
+*/
+
+let allCollisionComponents = 
+{
+    "hurtPlayer" : hurtPlayer,
+    "hurtBoss" : hurtBoss,
+    "physicsCollision" : physicsCollision,
+    "flingCharacter" : flingCharacter,
+    "applyVelocity" : applyVelocity,
+    "despawnSelf" : despawnSelf,
+    "changePage" : changePage,
+}
+
+
+
+
+function hurtPlayer(collidingElement)
+{
+    return collidingElement
+}
+
+
+
+function hurtBoss(collidingElement)
+{
+    return collidingElement
+}
+
+
+
+function physicsCollision(collidingElement)
+{
+    return collidingElement
+}
+
+
+
+function flingCharacter(collidingElement, direction)
+{
+    return collidingElement
+}
+
+
+
+function applyVelocity(collidingElement, x, y)
+{
+    return collidingElement
+}
+
+
+
+function despawnSelf(collidingElement)
+{
+    return collidingElement
+}
+
+
+
+function changePage(collidingElement, pageUrl)
+{
+    return collidingElement
+}
+
+
+
+
+
