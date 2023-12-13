e573199838e0e5646020e94f977b2234d96fb4b0
BobTheNerd10 <Bob_The_Nerd@shaw.ca>
2023-12-13 09:26:00 -0800

    base microgame class is fully setup!

--- public/zFinalProject/microgames.js
+++ public/zFinalProject/microgames.js
@@ -9,7 +9,15 @@
 // Start the game
 function microgamesSceneSequence()
 {
+    /*
+        Create the event list
+        Create new game manager
+        Set the parameters for the game manager
 
+        await processEventList
+
+        goto platformer_bossFight.html
+    */
 }
 
 
@@ -20,61 +28,89 @@ function microgamesSceneSequence()
 
 class GameManager
 {
-    constructor(floorList = [])
+    constructor(eventList = [])
     {
         this.lives = 4
-        this.difficulty = 1
         this.speed = 1 
         this.score = 0 // 0 = G, 1 = Floor 1, 2 = Floor 2, etc. There is no microgame on floor G
-        this.floorList = floorList // ["floorname", "speed+1", "floorname", "boss", "floornameBoss"]
-        this.currentFloorListIndex = 0
-        this.interruptOnNextFloor = false // Shouldnt have to use this, but just in case
+        this.eventList = eventList // ["floorname", "speed+1", "floorname", "boss", "floornameBoss"]
+        this.currentEventListIndex = 0
+        this.interruptOnNextEvent = false // Shouldnt have to use this, but just in case
     }
 
 
 
-    async processFloorList()
+    async processEventList()
     {
         
         // TODO: Play intro to minigame section
         // TODO: Play the music intro
 
-        while(this.interruptOnNextFloor == false)
+        while(this.interruptOnNextEvent == false)
         {
-            currentIndexActions = this.floorList[this.currentFloorListIndex].split(" ")
+            currentEvent = this.eventList[this.currentEventListIndex].split(" ")
+            // currentEvent is a list
+            // Index 0 is the type of event, every index after it is the parameters for the event (such as the promotion or the microgames)
 
             switch(currentIndexActions[0])
             {   
                 case "microgame":
                     currentIndexActions[1] // name of the microgame to get the microgame from the allmicrogames list
                     
-                    // Increase the score number and play its animation
-                    // jingle
-                    // Call microgame load function
-                    // Display splash text
-                    // Elevator doors open
+                    /*
 
-                    // Transfer control over to the microgame 
-                    // Load a bomb?
-                    // Await for microgame to finish by calling that finish function
+                    Get the microgame by name
+                    Call loadMicrogame function from the microgame (loads the game behind the elevator and returns the splashtext)
                     
-                    // Close elevator doors
+                    Play jingle and jingle animation
+                    Increase the score number and play the number animation during the jingle
+
+                    Show the splashtext
+                    open the elevator doors
+
+                    call MicrogameStart
                     
-                    // if win, play win sound, and win jingle
-                    // if lose, play lose sound and lose jingle, and decrease lives
+                    get the timer variable from the microgame
+
+                    until time = 0    
+                        Tick down the timer and spawn the timer bomb if needed
+
+                    MicrogameEnd()
+
+                    Elevator doors close
+
+                    unload microgame
+
+                    if PlayerHasWon()
+                        play win sound, and win jingle
+                    else
+                        play lose sound and lose jingle, and decrease lives
+
+                    */
                     
                     break
+
+
+
                 case "promotion":
                     currentIndexActions[1] // "+1" "1" "-1"
                     currentIndexActions[2] // "YourNewTitle"
+
+
                     // Play the promotion animation and jingle
+                    // It has coffee in it hehe
                     break
+
+
+                    
                 case "end":
-                    // Increase the score number and play its animation
-                    // Play the boss animation
-                    // Play the boss animation music
-                    // End the game and transition to platformer_bossFight.html
-                    break
+                    // Increase the score number and play the number animation (in practice, it'll happen on floor 20)
+                    // Play some sort of transition
+                    // End the game by returning
+                    return
+
+
+
                 default:
                     console.error(`Invalid FloorList action: ${currentIndexActions[0]}`)
                     break
@@ -100,20 +136,12 @@ class GameManager
 
 
 
-// TODO: figure out how to structure da microgames
-
-
-
-
-
 
 
 let AllMicrogames = 
 {
-    // "MicrogameName" : new Microgame(dsklfj, sdlfj)
+    // "MicrogameName" : new MICROGAMECLASS()
 }
-
-
 
 
 
@@ -123,37 +151,69 @@ let AllMicrogames =
 
 
 /*
-Floor/Microgame class:
-    - Floor element that is a div that fills the whole screen
-        - Contains all the game elements
-
-    - Timer integer in beats(make the default value be inherited from the gameManager)
-
-    - An async function that is called to load the floor 
-        - Loads the floor's assets
-        - Returns the splashtext for the floor (so the splashtext can be dynamic)
-
-    - An async function that is called when the floor starts
-        - Takes the floor's difficulty as an input
-        - Takes the floor's speed as an input 
-        - Should play the music dependant on the speed
-
-    - An async function that is called whevener the timer increments or decrements
-        - Takes the current time as an input (increases from 0 if the floor has no time limit) (decreases to 0 if the floor has a time limit)
-
-    - An async function that is called that returns whether the player has won or lost the game 
-        - called when time is up for timed floors
-        - called immediately, then awaited to signify the end of the floor for nontimed floors
-        - returns a bool for whether the player has won or lost the game
-    
-    - An async function that is called when the floor is hidden by the elevators and is ready to be unloaded
-        - unload the floor's assets
+================================================================================================================================================================
+Microgame classes beyond this point   Microgame classes beyond this point   Microgame classes beyond this point   Microgame classes beyond this point   
+================================================================================================================================================================
 */
+
+
+
+
+
+// Base microgame class (never used in of itself. Only used as a base)
 class Microgame
 {
-    constructor()
+    constructor(htmlDiv)
+    {
+        this.htmlDiv = htmlDiv
+    }
+
+    timer = 7
+    splashtext = "Do the thing!"
+    
+
+    // Called when the elevator doors are still closed (at the start of the jingle before the microgame)
+    loadMicrogame()
+    {
+        // Loads the floor's assets and makes them visible behind the elevator door
+        // This can set the timer variable and splashtext variable if needed
+    }
+
+
+    // Called when the elevator doors are fully open
+    microgameStart()
+    {
+        // play the microgame's music and do any other microgame initialization
+    }
+
+
+    // Called every timer tick. Can be used to sync up stuff in the game with the timer
+    timerUpdate(timerTick = this.timer)
     {
 
     }
+
+    // Called when the timer tick = 0, IE when the bomb explodes, IE when the game is over and the doors are about to close
+    microgameEnd()
+    {
+
+    }
+
+
+    get playerHasWon()
+    {
+        return true // TODO change me to be the conditional
+    }
+
+
+    // Called when doors are fully closed and the win/loss jinge is playing
+    // Hide the microgame's assets and html div
+    unloadMicrogame()
+    {
+
+    }
+
 }
+
+
 
